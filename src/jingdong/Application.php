1df61c75f1e6c3c8ac41030204a3792e8ea8a2c2
jianzi0307 <jianzi0307@qq.com>
2020-07-14 12:11:57 +0800

    feat: 拼多多goodsDetail接口添加pid,search_id等字段

--- src/jingdong/Application.php
+++ src/jingdong/Application.php
@@ -60,26 +60,24 @@ class Application
      */
     public static function __callStatic($name, $arguments)
     {
-        $obj = self::getInstance ();
+        $obj = self::getInstance();
         return $obj->$name($arguments);
     }
 
 
     /**
      * 执行
-     * @Author: niugengyun
-     * @Date: 2018/4/26
      * @param $request
      * @return mixed
+     * @author: niugengyun
+     * @date: 2018/4/26
      */
     public function execute($request, $access_token = null)
     {
-        $timestamp = date ('Y-m-d H:i:s');
-
-
+        $timestamp = date('Y-m-d H:i:s');
         $params = [
             'v' => $this->v,
-            'method' => $request->getMethod (),
+            'method' => $request->getMethod(),
             'app_key' => $this->appKey,
             'sign_method' => $this->signMethod,
             'format' => $this->format,
@@ -88,24 +86,23 @@ class Application
         if (null != $access_token) {
             $params["access_token"] = $access_token;
         }
+        $params['param_json'] = $request->getParamJson();;
 
-        $params['param_json'] = $request->getParamJson ();;
 
-
-        $params['sign'] = $this->generateSign ($params);
+        $params['sign'] = $this->generateSign($params);
 
 
         $requestUrl = $this->gatewayUrl . "?";
         foreach ($params as $k => $v) {
-            $requestUrl .= "$k=" . urlencode ($v) . '&';
+            $requestUrl .= "$k=" . urlencode($v) . '&';
         }
 
 
-        $requestUrl = substr ($requestUrl, 0, -1);
+        $requestUrl = substr($requestUrl, 0, -1);
 
 
-        $resp = $this->curl_post ($requestUrl);
-        $respArr = json_decode ($resp, true);
+        $resp = $this->curl_post($requestUrl);
+        $respArr = json_decode($resp, true);
         return $respArr;
     }
 
@@ -123,16 +120,16 @@ class Application
      */
     public function generateSign($params)
     {
-        ksort ($params);
+        ksort($params);
         $stringToBeSigned = $this->appSecret;
         foreach ($params as $k => $v) {
-            if (!is_array ($v) && "@" != substr ($v, 0, 1)) {
+            if (!is_array($v) && "@" != substr($v, 0, 1)) {
                 $stringToBeSigned .= "$k$v";
             }
         }
         unset($k, $v);
         $stringToBeSigned .= $this->appSecret;
-        return strtoupper (md5 ($stringToBeSigned));
+        return strtoupper(md5($stringToBeSigned));
     }
 
     /**
@@ -144,35 +141,35 @@ class Application
      */
     public function curl_post($url)
     {
-        $ch = curl_init ();
-        curl_setopt ($ch, CURLOPT_URL, $url);
-        curl_setopt ($ch, CURLOPT_FAILONERROR, false);
-        curl_setopt ($ch, CURLOPT_RETURNTRANSFER, true);
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_FAILONERROR, false);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         if ($this->readTimeout) {
-            curl_setopt ($ch, CURLOPT_TIMEOUT, $this->readTimeout);
+            curl_setopt($ch, CURLOPT_TIMEOUT, $this->readTimeout);
         }
         if ($this->connectTimeout) {
-            curl_setopt ($ch, CURLOPT_CONNECTTIMEOUT, $this->connectTimeout);
+            curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $this->connectTimeout);
         }
         //https 请求
-        if (strlen ($url) > 5 && strtolower (substr ($url, 0, 5)) == "https") {
-            curl_setopt ($ch, CURLOPT_SSL_VERIFYPEER, false);
-            curl_setopt ($ch, CURLOPT_SSL_VERIFYHOST, false);
+        if (strlen($url) > 5 && strtolower(substr($url, 0, 5)) == "https") {
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
         }
 
-        curl_setopt ($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POST, true);
 
-        $reponse = curl_exec ($ch);
+        $reponse = curl_exec($ch);
 
-        if (curl_errno ($ch)) {
-            throw new \Exception(curl_error ($ch), 0);
+        if (curl_errno($ch)) {
+            throw new \Exception(curl_error($ch), 0);
         } else {
-            $httpStatusCode = curl_getinfo ($ch, CURLINFO_HTTP_CODE);
+            $httpStatusCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
             if (200 !== $httpStatusCode) {
-                abort ($httpStatusCode);
+                abort($httpStatusCode);
             }
         }
-        curl_close ($ch);
+        curl_close($ch);
         return $reponse;
     }
 
--- src/pinduoduo/request/DdkGoodsDetailRequest.php
+++ src/pinduoduo/request/DdkGoodsDetailRequest.php
@@ -1,8 +1,8 @@
 <?php
+
 namespace NiuGengYun\EasyTBK\PinDuoDuo\Request;
 
 use NiuGengYun\EasyTBK\PinDuoDuo\RequestInterface;
-
 
 class DdkGoodsDetailRequest implements RequestInterface
 {
@@ -18,6 +18,50 @@ class DdkGoodsDetailRequest implements RequestInterface
      */
     private $goodsIdList;
 
+    /**
+     * 推广位id
+     * @var
+     */
+    private $pid;
+
+    /**
+     * 自定义参数
+     * @var
+     */
+    private $customParameters;
+
+    /**
+     * 搜索id，建议填写，提高收益。来自pdd.ddk.goods.recommend.get、pdd.ddk.goods.search、pdd.ddk.top.goods.list.query等接口
+     * @var
+     */
+    private $searchId;
+
+    /**
+     * 招商多多客ID
+     * @var
+     */
+    private $zsDuoId;
+
+    public function setCustomParameters($value)
+    {
+        $this->customParameters = $value;
+    }
+
+    public function setPid($pid)
+    {
+        $this->pid = $pid;
+    }
+
+    public function setSearchId($value)
+    {
+        $this->searchId = $value;
+    }
+
+    public function setZsDuoId($value)
+    {
+        $this->zsDuoId = $value;
+    }
+
     public function setType($type)
     {
         $this->type = $type;
@@ -28,6 +72,25 @@ class DdkGoodsDetailRequest implements RequestInterface
         $this->goodsIdList = $goodsIdList;
     }
 
+    public function getPid()
+    {
+        return $this->pid;
+    }
+
+    public function getSearchId()
+    {
+        return $this->searchId;
+    }
+
+    public function getZsDuoId()
+    {
+        return $this->zsDuoId;
+    }
+
+    public function getCustomParameters()
+    {
+        return $this->customParameters;
+    }
 
     public function getType()
     {
@@ -42,8 +105,12 @@ class DdkGoodsDetailRequest implements RequestInterface
     public function getParams()
     {
         return [
-            'type'          => $this->type,
-            'goods_id_list' => $this->goodsIdList
+            'type' => $this->type,
+            'goods_id_list' => $this->goodsIdList,
+            'pid' => $this->pid,
+            'custom_parameters' => $this->customParameters,
+            'search_id' => $this->searchId,
+            'zs_duo_id' => $this->zsDuoId
         ];
     }
 }
